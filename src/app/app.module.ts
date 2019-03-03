35afdc1a3f1966d353183791026e1e2ca9bf93ce
Pavlo Figol <pavlofigol@gmail.com>
2019-03-03 14:29:07 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { LoginComponent } from './login/login.component';
+import { ComicsDetailsComponent } from './comics-details/comics-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    LoginComponent,
+    ComicsDetailsComponent
   ],
   imports: [
     BrowserModule,
